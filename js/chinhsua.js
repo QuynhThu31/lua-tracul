fd39da9af0c74c4e34eff78dda70e1e2543f4321
QuynhThu31 <63961082+QuynhThu31@users.noreply.github.com>
2020-04-23 22:00:07 +0700

    Add files via upload

---
+++ js/chinhsua.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+
+function show(a) {
+  var x=document.getElementById(a);
+  var c=x.nextElementSibling
+  if (x.getAttribute('type') == "password") {
+  c.removeAttribute("class");
+  c.setAttribute("class","fas fa-eye");
+  x.removeAttribute("type");
+    x.setAttribute("type","text");
+  } else {
+  x.removeAttribute("type");
+    x.setAttribute('type','password');
+ c.removeAttribute("class");
+  c.setAttribute("class","fas fa-eye-slash");
+  }
+}
